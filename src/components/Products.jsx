e07344705125818f90347441a7ce2a049c451ee1
Jimmy Signature <jimmysignature@gmail.com>
2023-08-25 20:53:06 -0400

    add routes

---
+++ src/components/Products.jsx
@@ -0,0 +1,6 @@
+const Products = () => {
+  console.log('test1')
+  return <h1>Products</h1>
+}
+
+export default Products
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+
 import './index.css'
+import Products from './components/Products.jsx'
+import Cart from './components/Cart.jsx'
+import CheckOut from './components/CheckOut.jsx'
+import LogIn from './components/LogIn.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<CheckOut />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
